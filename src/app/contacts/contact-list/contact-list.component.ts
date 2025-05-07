79a31e0519552cb29c7943543504cee4f07a58c8
Timothy Harris <timothy.harris@kikoda.com>
2025-05-07 15:57:41 -0400

    fix: added config for assets folder and updated file references

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit, } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { Contact } from "../contact.model";
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
   imports: [CommonModule],
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  styleUrl: './contact-list.component.css',
 })
 export class ContactListComponent implements OnInit {
   contacts: Contact[] = [
-    new Contact("1", "R. Kent Jackson", "rkjackson@byui.edu", "208-496-3771", "../../assets/images/jacksonk.jpg"),
-    new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/images/barzeer.jpg"),
+    new Contact(
+      '1',
+      'R. Kent Jackson',
+      'rkjackson@byui.edu',
+      '208-496-3771',
+      'images/jacksonk.jpg'
+    ),
+    new Contact(
+      '2',
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      'images/barzeer.jpg'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
